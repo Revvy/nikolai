629cea77c6930b89c5f68917bb270997ad489ca4
Revvy <reverend.null@gmail.com>
2009-05-22 21:10:50 -0700

    Support for multiple networks, better plugin system, cleaner. Works.

--- bot.py
+++ bot.py
@@ -1,94 +1,69 @@
+import socket
+import select
 import settings
-
-class Tokenize(object):
-    def __init__(self, data, addressed, ident):
-        """
-        A slimmed down version of Tokenizer.Data. data should be an re.match of
-        a PRIVMSG
-        """
-        self.addressed = addressed
-        self.ident = ident
-
-        self.nick = data.group(1)
-        self.username = data.group(2)
-        self.host = data.group(3)
-        self.channel = data.group(5)
-        self.message = data.group(6)
-
-        if self.channel.find("#") != -1:
-            self.reply_to = channel
-        else:
-            self.reply_to = nick
-
-    def has_key(self, key):
-        """
-        Returns True if self.message has key
-        sets self.args to the string following key.
-        """
-        if not self.addressed:
-            key = settings.COMMAND_TOKEN + key #Want to get rid of this and not have settings in this modules.
-
-        f = self.message.find(key)
-        if f != -1:
-            self.args = self.message[f+len(key)+1:]
-            return True
-        
+import functions
 
 class Bot(object):
     def __init__(self):
-        self.messages = []
+        self.sockets = []
+        self.nicks = {} #Replace this with a get_nicks() method
+        self.commands = self.load_plugins()
         self.masters = {}
-        self.commands = self.get_commands()
+        self.password = settings.PASSWORD
+        self.command_token = settings.COMMAND_TOKEN
 
-    def get_commands(self):
-        """
-        This should import all the .py files in /plugins and run module.get_commands()
-        for each one. module.get_commands() returns a list of functions that gets
-        added to self.commands
-        """
-        pass
-        
-
-
-    def parse(self, sock, data):
-        """
-        Replacement for Tokenizer.Data. There's so much shared functionality
-        between Bot and Tokenizer.Data that it makes more sense to have parsing
-        incoming messages be a method of Bot rather than passing Bot over every
-        message.
-        """
-
-        if re.search('^:(.+)!(.+)@(.+) (.+) (.+) :(.+)$', data):
-            m = re.search('^:(.+)!(.+)@(.+) (.+) (.+) :(.+)$', data)
-            
-            if data.find(self.nicks[id(sock)]) != -1:
-                addressed = True
-            else:
-                addressed = False
-                
+    def connect(self):
+        for network in settings.networks:
+            sock = socket.socket()
             try:
-                self.masters[m.group(3)] #host
-                ident = True
+                port = network['port']
             except:
-                ident = False
-                
-            data = Tokenize(m, addressed, ident)
+                port = 6667
+            sock.connect((network['host'], port))
 
-            #Here we run the commands.
+            try:
+                nick = network['nick']
+            except:
+                nick = settings.defaultNick
+            self.handle_output(sock, 'NICK %s\r\n' % nick)
+            #Should try altNick is user is in use.
+            self.nicks[id(sock)] = nick #Replace this with get_nick()
+
+            try:
+                user = network['user']
+            except:
+                user = settings.defaultUser
+            self.handle_output(sock, 'USER %s\r\n' % user)
+
+            for channel in network['channels']:
+                self.handle_output(sock, 'JOIN %s %s\r\n' % (channel[0], channel[1]))
+
+            self.sockets.append(sock)
+
+        while 1:
+            read, write, exception = select.select(self.sockets,[],[])
+            for sock in read:
+                #try:
+                data = sock.recv(4096)
+                self.handle_input(sock, data)
+                #except:
+                #    print 'Something went wrong'
+
+    def handle_input(self, sock, data):
+        functions.handle_input(self, sock, data)
+
+    def handle_output(self, sock, data):
+        functions.handle_output(self, sock, data)
+
+    def load_plugins(self):
+        return functions.load_plugins(self)
+
+    def reload_bot(self):
+        reload(functions)
 
 
-        elif re.search('^PING :(.*)$', self.raw_data):
-            sock.send('PONG')
 
-        #We aren't really doing anything with these last two but I'll keep them in.
-        elif re.search('^:(.+) ([0-9]+) (.+)$', self.raw_data):
-            m = re.search('^:(.+) ([0-9]+) (.+)$', self.raw_data)
-            self.host = m.group(1)
-            self.type = 'SERVMSG'
-            self.code = m.group(2)
-            self.message = m.group(3)
 
-        else:
-            self.type = 'OTHER'
-
-bot = Bot()
+if __name__ == '__main__':
+    bot = Bot()
+    bot.connect()
--- settings.py
+++ settings.py
@@ -6,9 +6,10 @@ defaultUser = 'py py py py py bot'
 
 #Networks must have 'host' and 'channels'. 'nick' and 'user' are optional
 networks = [
-    {'host':'irc.adelais.net', 'channels':(('#omgabot',''))},
-    {'host':'irc.efnet.net', 'channels':(('#omgabot', '')), 'nick':'hurdurian'},
+    {'host':'irc.accessirc.net', 'channels':(('#omgabot',''),)},
+    {'host':'irc.accessirc.net', 'channels':(('#omgabot',''),), 'nick':'hurdurian'},
     ]
 
 #Bot settings go here.
 COMMAND_TOKEN = '@'
+PASSWORD = 'password'
